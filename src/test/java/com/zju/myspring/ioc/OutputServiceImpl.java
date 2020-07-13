79af9b679dba9aa4eb9c6a045b679aeb6911ab30
1216676256 <835379639@qq.com>
2020-07-13 11:15:02 +0800

    终结

--- src/test/java/com/zju/myspring/ioc/OutputServiceImpl.java
+++ src/test/java/com/zju/myspring/ioc/OutputServiceImpl.java
@@ -7,6 +7,7 @@ import com.zju.myspring.ioc.OutputService;
  * @CreateTime 2020/2/3 13:39
  */
 public class OutputServiceImpl implements OutputService {
+    HelloServiceImpl helloService;
     @Override
     public void printByReference(String text) {
         System.out.println("通过引用调用输出   " + text);
